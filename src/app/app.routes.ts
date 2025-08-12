14f52aef417e11b4dd5d1f80542516ea3b7b0cf0
alfayedtch <albos7484@gmail.com>
2025-08-12 19:59:16 +0200

    add login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,13 @@ export const routes: Routes = [
     path: 'home',
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
+   {
+    path: 'login',
+    loadComponent: () => import('./login/login.page').then((m) => m.LoginPage),
+  },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full',
   },
 ];
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,15 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 
+import { provideAnimations } from '@angular/platform-browser/animations';
+
+
 bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideAnimations() // <-- important pour les animations Angular
+
   ],
 });
